284de5c46b39ff1cb6057b2025e3c2045a22ade5
comrern <kb22541@bristol.ac.uk>
2025-05-27 12:08:50 +0100

    minor changes to plots

--- plots.R
+++ plots.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(TwoSampleMR)
 
-data <- read.table("/Users/kb22541/Desktop/Analyses/simulation/mvmr_simulations/results/avergaed_results_fullsims.csv")
+data <- read.table("/Users/kb22541/Desktop/Analyses/simulation/mvmr_simulations/results/avg_test_ld_exp.csv")
 data <- generate_odds_ratios(data)
 
 data <- data[!(data$exposure==2),]
@@ -13,22 +13,21 @@ data$labels <- c("IVW - exposure 1", "MVMR - exposure 1","IVW - exposure 1", "MV
 ### subplots
 
 
-for (mode in c(1,2,3,4)){
+for (mode in c("TRUE","FALSE")){
   
-  mode_dat <- data[data$setup_mode == mode,]
+  mode_dat <- data[data$LD_mod == mode,]
   
-  combined_plot <- ggplot(mode_dat, aes(y=(key), x=(or),xmin=(or_lci95), xmax=(or_uci95),group=model))
+  combined_plot <- ggplot(mode_dat, aes(y=(key), x=(b),xmin=(lo_ci), xmax=(up_ci),group=model))
   
   
   print(combined_plot + geom_point() +
-    xlab("Odds ratio") + 
+    xlab("beta") + 
     theme_bw() +
     geom_errorbarh(height=.1) +
-    geom_vline(xintercept=1) +
+    geom_vline(xintercept=0) +
     scale_y_discrete(drop=T, labels=data$labels) +  
     facet_wrap(~ model, scale= "free_y") +
-    ggtitle(paste("Setup mode", mode))) +
-    theme(legend.position="none")
+    theme(legend.position="none"))
    
   
 }
