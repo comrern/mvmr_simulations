83a1d36b942d49b2497a4e6a4f8b70992bebbb10
comrern <kb22541@bristol.ac.uk>
2025-05-27 11:01:43 +0100

    plot shenans

--- plots.R
+++ plots.R
@@ -1,35 +1,45 @@
 library(ggplot2)
 library(TwoSampleMR)
 
-data <- read.table("/Users/kb22541/Desktop/Analyses/simulation/mvmr_simulations/results/avergaed_results_fullsims.csv")
-data <- generate_odds_ratios(data)
+data <- read.table("/Users/kb22541/Desktop/Analyses/simulation/mvmr_simulations/results/avergaed_results_fullsims.csv", sep=",", header=T)
 
-data <- data[!(data$exposure==2),]
+data <- data[!(data$Exposure==2),]
 
-data$key <- paste0(data$model,"-", data$method ,"-",data$exposure)
+data$key <- paste0(data$Model,"-", data$Method ,"-",data$Exposure)
 
-data$labels <- c("IVW - exposure 1", "MVMR - exposure 1","IVW - exposure 1", "MVMR - exposure 1", "IVW - exposure 1", "MVMR - exposure 1", "IVW - exposure 1", "MVMR - exposure 1")
+data$labels <- c("IVW - Exposure 1", "MVMR - Exposure 1","IVW - Exposure 1", "MVMR - Exposure 1", "IVW - Exposure 1", "MVMR - Exposure 1", "IVW - Exposure 1", "MVMR - Exposure 1")
+
+data$lci95 <- data$beta - (data$se * 1.96)
+data$uci95 <- data$beta + (data$se * 1.96)
+
 
 ### subplots
 
 
-for (mode in c(1,2,3,4)){
+for (mode in c(1,2,3,4)) {
   
-  mode_dat <- data[data$setup_mode == mode,]
+  mode_dat <- data[data$setup.mode == mode, ]
   
-  combined_plot <- ggplot(mode_dat, aes(y=(key), x=(or),xmin=(or_lci95), xmax=(or_uci95),group=model))
+  # Create a dataframe to hold vertical lines for Models B and D
+  vline_data <- data.frame(
+    xintercept = 0.4,
+    Model = c("B", "D")
+  )
   
-  
-  print(combined_plot + geom_point() +
-    xlab("Odds ratio") + 
+  combined_plot <- ggplot(mode_dat, aes(y = key, x = beta, xmin = lci95, xmax = uci95, group = model)) +
+    geom_point() +
+    xlab("Beta") + 
     theme_bw() +
-    geom_errorbarh(height=.1) +
-    geom_vline(xintercept=1) +
-    scale_y_discrete(drop=T, labels=data$labels) +  
-    facet_wrap(~ model, scale= "free_y") +
-    ggtitle(paste("Setup mode", mode))) +
-    theme(legend.position="none")
-   
+    geom_errorbarh(height = 0.1) +
+    geom_vline(xintercept = 0) +
+    geom_vline(data = vline_data, aes(xintercept = xintercept), 
+               linetype = "dashed", color = "red") +
+    scale_y_discrete(drop = TRUE, labels = data$labels) +  
+    facet_wrap(~ Model, scales = "free_y") +
+    ggtitle(paste("Setup mode", mode)) +
+    theme(legend.position = "none")
   
+  print(combined_plot)
 }
+
 
