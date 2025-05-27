4a5469bfe2083350d51762c9735cf16aef95cc36
comrern <kb22541@bristol.ac.uk>
2025-05-27 15:25:32 +0100

    pretty multicoloured plot

--- plots.R
+++ plots.R
@@ -1,45 +1,98 @@
 library(ggplot2)
 library(TwoSampleMR)
 
-data <- read.table("/Users/kb22541/Desktop/Analyses/simulation/mvmr_simulations/results/avergaed_results_fullsims.csv", sep=",", header=T)
+data <- read.table("/Users/kb22541/Desktop/Analyses/simulation/mvmr_simulations/results/avergaed_results_fullsims.csv", header=T)
+data <- data[1:48,]
 
-data <- data[!(data$Exposure==2),]
+data <- data[!(data$exposure==2),]
 
-data$key <- paste0(data$Model,"-", data$Method ,"-",data$Exposure)
+data$key <- paste0(data$model,"-", data$method ,"-",data$exposure)
 
 data$labels <- c("IVW - Exposure 1", "MVMR - Exposure 1","IVW - Exposure 1", "MVMR - Exposure 1", "IVW - Exposure 1", "MVMR - Exposure 1", "IVW - Exposure 1", "MVMR - Exposure 1")
 
-data$lci95 <- data$beta - (data$se * 1.96)
-data$uci95 <- data$beta + (data$se * 1.96)
+data$lci95 <- data$b - (data$se * 1.96)
+data$uci95 <- data$b + (data$se * 1.96)
 
 
 ### subplots
 
 
-for (mode in c(1,2,3,4)) {
-  
-  mode_dat <- data[data$setup.mode == mode, ]
-  
-  # Create a dataframe to hold vertical lines for Models B and D
-  vline_data <- data.frame(
-    xintercept = 0.4,
-    Model = c("B", "D")
+# for (mode in c(1,2,3,4)) {
+#   
+#   mode_dat <- data[data$setup.mode == mode, ]
+#   
+#   # Create a dataframe to hold vertical lines for Models B and D
+#   vline_data <- data.frame(
+#     xintercept = 0.4,
+#     Model = c("B", "D")
+#   )
+#   
+#   combined_plot <- ggplot(mode_dat, aes(y = key, x = beta, xmin = lci95, xmax = uci95, group = model)) +
+#     geom_point() +
+#     xlab("Beta") + 
+#     theme_bw() +
+#     geom_errorbarh(height = 0.1) +
+#     geom_vline(xintercept = 0) +
+#     geom_vline(data = vline_data, aes(xintercept = xintercept), 
+#                linetype = "dashed", color = "red") +
+#     scale_y_discrete(drop = TRUE, labels = data$labels) +  
+#     facet_wrap(~ Model, scales = "free_y") +
+#     ggtitle(paste("Setup mode", mode)) +
+#     theme(legend.position = "none")
+#   
+#   print(combined_plot)
+# }
+
+
+## ai slop code:
+# Assume 'key' is something like 'Method1_A', 'Method2_A', etc.
+# First, extract 'method' grouping for spacing
+data <- data %>%
+  mutate(
+    method_group = sub("_.*", "", key)  # e.g., 'Method1' from 'Method1_A'
   )
-  
-  combined_plot <- ggplot(mode_dat, aes(y = key, x = beta, xmin = lci95, xmax = uci95, group = model)) +
-    geom_point() +
-    xlab("Beta") + 
-    theme_bw() +
-    geom_errorbarh(height = 0.1) +
-    geom_vline(xintercept = 0) +
-    geom_vline(data = vline_data, aes(xintercept = xintercept), 
-               linetype = "dashed", color = "red") +
-    scale_y_discrete(drop = TRUE, labels = data$labels) +  
-    facet_wrap(~ Model, scales = "free_y") +
-    ggtitle(paste("Setup mode", mode)) +
-    theme(legend.position = "none")
-  
-  print(combined_plot)
-}
 
+# Create spaced key_id with gaps between method groups
+data <- data %>%
+  group_by(method_group, key) %>%
+  summarise(.groups = "drop") %>%
+  arrange(method_group, key) %>%
+  mutate(key_id = row_number() + cumsum(c(0, diff(as.numeric(factor(method_group))) != 0))) %>%
+  right_join(data, by = c("key", "method_group"))
 
+# Offset y by setup.mode to avoid overlap
+data <- data %>%
+  mutate(
+    setup_mode_offset = (as.numeric(factor(setup_mode)) - 2.5) * 0.25,
+    y_pos = key_id + setup_mode_offset
+  )
+
+# Y-axis labels at central position per key
+y_labels <- data %>%
+  distinct(key, key_id)
+
+# Red line only in models B and D
+vline_data <- data.frame(
+  xintercept = 0.4,
+  model = c("B", "D")
+)
+
+# Plot
+combined_plot <- ggplot(data, aes(y = y_pos, x = b, xmin = lci95, xmax = uci95, color = factor(setup_mode))) +
+  geom_point() +
+  geom_errorbarh(height = 0.15) +
+  geom_vline(xintercept = 0, color = "black") +
+  geom_vline(data = vline_data, aes(xintercept = xintercept), 
+                         linetype = "dashed", color = "red") +
+  scale_y_continuous(
+    breaks = y_labels$key_id,
+    labels = y_labels$key
+  ) +
+  facet_wrap(~ model, scales = "free_y") +
+  xlab("Beta") +
+  ylab("Key") +
+  ggtitle("Combined Results by Setup Mode") +
+  theme_bw() +
+  labs(color = "Setup Mode")
+
+print(combined_plot)
